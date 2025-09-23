fb1ea89a53be15f1b3d5cbedc5e908ea48cdbcaf
Samuel Malec <malec293@gmail.com>
2025-09-23 20:32:07 +0200

    feat: enhance code generation with statement and expression handling

--- tinyc/src/codegen/codegen.cpp
+++ tinyc/src/codegen/codegen.cpp
@@ -4,15 +4,89 @@ namespace tinyc::codegen {
 void CodeGen::generate(const std::vector<parser::StmtPtr> &statements)
 {
     for (const auto &stmt : statements)
-        genStmt(stmt);
+        gen_stmt(stmt);
 }
 
-void CodeGen::genStmt(const std::unique_ptr<parser::Stmt> &stmt)
+void CodeGen::gen_stmt(const parser::StmtPtr &stmt)
 {
-    
+
+    if (const auto s = dynamic_cast<parser::ExprStmt *>(stmt.get()))
+    {
+        gen_expr(s->expr);
+        return;
+    }
+
+    if (const auto s = dynamic_cast<parser::CompoundStmt *>(stmt.get()))
+    {
+        // TODO
+    }
+
+    if (const auto s = dynamic_cast<parser::ReturnStmt *>(stmt.get()))
+    {
+        // TODO
+    }
+    if (const auto s = dynamic_cast<parser::IfStmt *>(stmt.get()))
+    {
+        // TODO
+    }
+    if (const auto s = dynamic_cast<parser::WhileStmt *>(stmt.get()))
+    {
+        // TODO
+    }
+    if (const auto s = dynamic_cast<parser::ForStmt *>(stmt.get()))
+    {
+        // TODO
+    }
+    if (const auto s = dynamic_cast<parser::VarDeclStmt *>(stmt.get()))
+    {
+        // TODO
+    }
+    if (const auto s = dynamic_cast<parser::FunctionDeclStmt *>(stmt.get()))
+    {
+        // TODO
+    }
+
+    if (const auto s = dynamic_cast<parser::StructDeclStmt *>(stmt.get()))
+    {
+        // TODO
+    }
 }
 
-void CodeGen::genExpr(const std::unique_ptr<parser::Expr> &expr)
+void CodeGen::gen_expr(const parser::ExprPtr &expr)
+{
+    if (const auto e = dynamic_cast<parser::LiteralExpr *>(expr.get()))
+    {
+        // TODO
+    }
+    if (const auto e = dynamic_cast<parser::VariableExpr *>(expr.get()))
+    {
+        // TODO
+    }
+    if (const auto e = dynamic_cast<parser::UnaryExpr *>(expr.get()))
+    {
+        // TODO
+    }
+    if (const auto e = dynamic_cast<parser::BinaryExpr *>(expr.get()))
+    {
+        // TODO
+    }
+    if (const auto e = dynamic_cast<parser::AssignmentExpr *>(expr.get()))
+    {
+        // TODO
+    }
+    if (const auto e = dynamic_cast<parser::CallExpr *>(expr.get()))
+    {
+        // TODO
+    }
+}
+
+llvm::AllocaInst *CodeGen::create_entry_block_alloca(llvm::Function *   func,
+                                                     const std::string &var_name, llvm::Type *type)
 {
 }
+
+llvm::Type *CodeGen::get_llvm_type_from_token(const lexer::Token &type_token)
+{
+}
+
 }
--- tinyc/src/codegen/codegen.hpp
+++ tinyc/src/codegen/codegen.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "../parser/stmt.hpp"
+#include "../lexer/token.hpp"
 
+#include <map>
+#include <memory>
+#include <string>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
@@ -10,17 +14,23 @@
 namespace tinyc::codegen {
 struct CodeGen
 {
-    llvm::LLVMContext             context;
-    llvm::IRBuilder<>             builder;
-    std::unique_ptr<llvm::Module> module;
+    llvm::LLVMContext                    context;
+    llvm::IRBuilder<>                    builder;
+    std::unique_ptr<llvm::Module>        module;
+    std::map<std::string, llvm::Value *> named_values;
+
+    llvm::Function *current_function = nullptr;
 
     explicit CodeGen(const std::string &module_name)
         : builder(context), module(std::make_unique<llvm::Module>(module_name, context))
     {
     }
 
-    void generate(const std::vector<parser::StmtPtr> &statements);
-    void genStmt(const std::unique_ptr<parser::Stmt> &stmt);
-    void genExpr(const std::unique_ptr<parser::Expr> &expr);
+    void              generate(const std::vector<parser::StmtPtr> &statements);
+    void              gen_stmt(const parser::StmtPtr &stmt);
+    void              gen_expr(const parser::ExprPtr &expr);
+    llvm::AllocaInst *create_entry_block_alloca(llvm::Function *func, const std::string &var_name,
+                                                llvm::Type *    type);
+    llvm::Type *get_llvm_type_from_token(const lexer::Token &type_token);
 };
 }
