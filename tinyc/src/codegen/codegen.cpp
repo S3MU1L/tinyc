ec2ea63cd8a001ec11473890679f186375c8651d
Samuel Malec <malec293@gmail.com>
2025-09-28 22:45:21 +0200

    refactor: remove meaningless comments

--- tinyc/src/codegen/codegen.cpp
+++ tinyc/src/codegen/codegen.cpp
@@ -1,16 +1,11 @@
 #include "codegen.hpp"
 
 namespace tinyc::codegen {
-// This is an object that owns LLVM core data structures
 llvm::LLVMContext context;
 
-// This is a helper object that makes easy to generate LLVM instructions
 llvm::IRBuilder<> builder(context);
 
-// This is an LLVM construct that contains functions and global variables
 std::unique_ptr<llvm::Module> modules;
 
-// This map keeps track of which values are defined in the current scope
 std::map<std::string, llvm::Value *> named_values;
-
 } // namespace tinyc::codegen
--- tinyc/src/codegen/codegen.hpp
+++ tinyc/src/codegen/codegen.hpp
@@ -6,15 +6,11 @@
 #include <map>
 
 namespace tinyc::codegen {
-// This is an object that owns LLVM core data structures
 extern llvm::LLVMContext context;
 
-// This is a helper object that makes easy to generate LLVM instructions
 extern llvm::IRBuilder<> builder;
 
-// This is an LLVM construct that contains functions and global variables
 extern std::unique_ptr<llvm::Module> modules;
 
-// This map keeps track of which values are defined in the current scope
 extern std::map<std::string, llvm::Value *> named_values;
 }
